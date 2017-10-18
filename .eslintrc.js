d9c59bf6688a899400f6322841d088db79826a3b
Kristian Mandrup <kmandrup@gmail.com>
2017-10-18 19:54:25 +0200

    configure ava testing w babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,15 @@
 module.exports = {
-  // 'extends': 'airbnb',
-  // 'plugins': [
-  //   'react'
-  // ],
   'parserOptions': {
-    'ecmaVersion': 6
+    'ecmaVersion': 6,
+    'sourceType': 'module'
   },
   'rules': {
     'arrow-parens': [2, 'as-needed'],
-    'brace-style': [2, 'stroustrup', {
-      'allowSingleLine': true
-    }],
     'semi': [2, "never"],
     'camelcase': 0,
-    // 'comma-dangle': [2, 'never'],
     'linebreak-style': 0,
     'max-len': [2, {
       code: 160
-    }],
-    'new-cap': [2, {
-      'capIsNewExceptions': ['AbstractApi', 'Repositories', 'Request', 'Teams', 'User']
     }]
   }
 };
