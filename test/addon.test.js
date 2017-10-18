a39b227aaf732f8e77f57301c8cca2241a360ef1
Kristian Mandrup <kmandrup@gmail.com>
2017-10-18 22:19:22 +0200

    better test setup

--- test/addon.test.js
+++ test/addon.test.js
@@ -2,6 +2,16 @@ import {
   test,
   Addon
 } from './imports'
+import {
+  prepareTest
+} from './prepare'
+
+const {
+  $get,
+  $post,
+  user,
+  repo
+} = prepareTest()
 
 test('Addon: create', t => {
   t.truthy(Addon)
--- test/repositories.test.js
+++ test/repositories.test.js
@@ -1,21 +1,52 @@
-import test from 'ava'
-import request from 'supertest'
 import {
+  test,
   Repositories
-} from '../src/repositories'
-import prepareApi from './prepare-api'
+} from './imports'
+import {
+  prepareTest
+} from './prepare'
+import {
+  createApi
+} from './prepare'
 
-let api
-test.before(t => {
-  api = prepareApi()
+const {
+  $get,
+  $post,
+  user,
+  repo
+} = prepareTest()
+
+const $api = createApi()
+const api = $api.repositories.promised
+
+function isFun(t, x) {
+  t.is(typeof x, 'function')
+}
+
+test.cb('Repositories: new', t => {
+  t.truthy(Repositories)
+  let reps = new Repositories(api)
+  // check api
+  isFun(t, reps.commit)
+
+  // check promised api
+  let project = reps.forProject(user, repo)
+  isFun(t, project.commit)
+
+  // check callback API
+  reps.get(user, repo, result => {
+    t.truthy(result)
+    t.end()
+  })
 })
 
-test('Repositories: create', t => {
+test('Repositories: get', async t => {
+  let result = await repositories.get(user, repo)
+})
+
+test('Repositories: create', async t => {
   t.truthy(Repositories)
 
-  // request
-  //   .get('/hello')
-  //   .expect(200)
+  let result = await $get(user, repo)
+    .expect(200)
 })
-
-// more tests ...
