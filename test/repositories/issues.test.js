df56074dca201a9f97924fc599a7dec4e67e6342
Kristian Mandrup <kmandrup@gmail.com>
2017-10-18 23:48:13 +0200

    more tests

--- test/repositories/issues.test.js
+++ test/repositories/issues.test.js
@@ -1,8 +1,40 @@
-import test from 'ava'
-import Issues from '../../src/repositories/hooks'
+import {
+  test,
+  Issues
+} from './imports'
 
-test('Issues: create', t => {
-  t.truthy(Issues)
+import {
+  $api,
+  prepareTest
+} from '../prepare'
+
+const api = $api.issues.promised
+let methods = ['getAll', 'get', 'remove']
+let $stubs = prepareTest(test, api, methods)
+
+test('Issues: getAll', async t => {
+  const expected = require('./mocks/hooks.json')
+  const node = '123'
+  $stubs.getAll.returns(Promise.resolve(expected))
+
+  let result = await api.getAll(user, repo, node)
+  t.truthy(result)
 })
 
-// more tests ...
+test('Issues: get', async t => {
+  const expected = require('./mocks/hooks.json')
+  const node = '123'
+  $stubs.get.returns(Promise.resolve(expected))
+
+  let result = await api.get(user, repo, node)
+  t.truthy(result)
+})
+
+test('Issues: remove', async t => {
+  const expected = require('./mocks/hooks.json')
+  const node = '123'
+  $stubs.remove.returns(Promise.resolve(expected))
+
+  let result = await api.remove(user, repo, node)
+  t.truthy(result)
+})
