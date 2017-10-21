8ece5e52e0da28f3d49f8d0859d3c34eec8ed1bf
Kristian Mandrup <kmandrup@gmail.com>
2017-10-21 11:14:23 +0200

    prepare for awesome test generation

--- test/helpers/generators/index.js
+++ test/helpers/generators/index.js
@@ -9,46 +9,70 @@ import {
 } from '../mock'
 import defaults from './defaults'
 
-function generateTest(test, config = {}) {
-  // generate test case using configuration
-  let testCase = {}
-  let {
-    expected,
-    methodName,
-    requestType,
-  } = config
+const {
+  log
+} = console
 
-  config.expect = expected[methodName]
+function createTestGenerator(test, template = {}) {
+  return new TestGenerator(test, template)
+}
 
-  test(config.label, async t => {
+class TestGenerator {
+  constructor(test, template = {}) {
+    this.test = test
+    this.template = template
+  }
+
+  get label() {
+    return `${this.template.$api}: ${this.method}`
+  }
+
+  generate(method) {
+    this.method = method
+    let config = this.template[method]
+    log('generateTest', {
+      test,
+      config
+    })
+    // generate test case using configuration
+    let testCase = {}
     let {
-      request,
-      execute,
-      body,
-      data
+      expected,
+      methodName,
+      requestType,
     } = config
 
-    const createComparer = config.createComparer || defaults.createComparer
-    const compare = createComparer(t, config)
+    config.expect = expected[methodName]
 
-    const mockConfig = requestType ? {
-      [requestType]: methodName
-    } : methodName
+    test(this.label, async t => {
+      let {
+        request,
+        execute,
+        body,
+        data
+      } = config
 
-    const prepareMock = config.prepareMock || mock.prepareMock
-    const mockOpts = {
-      data,
-      body
-    }
+      const createComparer = config.createComparer || defaults.createComparer
+      const compare = createComparer(t, config)
 
-    prepareMock(mockConfig, mockOpts)
-    const result = await execute(config)
-    compare(t, result)
-  })
-  return testCase
+      const mockConfig = requestType ? {
+        [requestType]: methodName
+      } : methodName
+
+      const prepareMock = config.prepareMock || mock.prepareMock
+      const mockOpts = {
+        data,
+        body
+      }
+
+      prepareMock(mockConfig, mockOpts)
+      const result = await execute(config)
+      compare(t, result)
+    })
+
+  }
 }
 
 module.exports = {
-  generateTest,
-  createComparer
+  createTestGenerator
 }
